1f9adf5c4cef5c8dd110f243e9c8ec33c106f711
BIGKINGS10 <adventbigkings@gmail.com>
2023-05-06 23:21:34 +0100

    made progress on the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("#tweet-text").keyup(function() {
+      let charRemaining = $(this).siblings(".counter")[0];
+      const maxLength = 140;
+      let currentLength = this.value.length;
+      document.getElementById('counter') = currentLength;
+  
+      $(charRemaining).text(maxLength - currentLength)
+      if(currentLength === maxLength) {
+        $(charRemaining).removeClass("black");
+        $(charRemaining).addClass("red");
+        
+      } else {
+        $(charRemaining).removeClass("red");
+        $(charRemaining).addClass("black");
+      }
+    })
+  
+  });
