1ec945d2aa68a68dbee9b441c14346aa4715643c
BIGKINGS10 <adventbigkings@gmail.com>
2023-05-07 08:41:58 +0100

    completed the character counter logic and styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $("#tweet-text").keyup(function() {
-      let charRemaining = $(this).siblings(".counter")[0];
+      let charRemaining = document.getElementById('counter').innerHTML;
       const maxLength = 140;
       let currentLength = this.value.length;
-      document.getElementById('counter') = currentLength;
-  
-      $(charRemaining).text(maxLength - currentLength)
-      if(currentLength === maxLength) {
-        $(charRemaining).removeClass("black");
-        $(charRemaining).addClass("red");
-        
-      } else {
-        $(charRemaining).removeClass("red");
-        $(charRemaining).addClass("black");
+      document.getElementById('counter').innerHTML = maxLength - currentLength;
+    $(charRemaining).text(maxLength - currentLength)
+      let element = document.getElementById("counter");
+    let diff = Number (maxLength - currentLength);
+        if(diff < 0) {
+        element.classList.remove("black");
+        element.classList.add("red");
+        } else {
+            element.classList.remove("red");
+            element.classList.add("black");
       }
     })
   
